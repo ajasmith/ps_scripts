ef358cfe8f1b47398e94f7ca54b7cb613075839a
Andrew Smith <aja.smith@gmail.com>
2026-07-28 13:05:02 +0100

    Update To-gif to prevent warning in ffmeg expecting image sequence for pallete

--- scripts/To-gif.ps1
+++ scripts/To-gif.ps1
@@ -7,9 +7,9 @@ Param (
 
 $item = (Get-Item $file)
 $o = $item.BaseName + ".gif"
-$tmpPalette = [System.IO.Path]::GetTempFileName() + ".png"
+$tmpPalette = [System.IO.Path]::ChangeExtension([System.IO.Path]::GetTempFileName(), ".png")
 
 Write-Output("Converting " + $item.Name + " -> " + $o + " using tmp palette " + $tmpPalette)
 
-ffmpeg -i $file -v warning -vf "fps=5,scale=640:-1:flags=lanczos,palettegen" -y $tmpPalette
+ffmpeg -i $file -v warning -vf "fps=5,scale=640:-1:flags=lanczos,palettegen" -update 1 -y $tmpPalette
 ffmpeg -i $file -i $tmpPalette -v warning -lavfi "fps=10,scale=640:-1:flags=lanczos,paletteuse" -an -y $o
